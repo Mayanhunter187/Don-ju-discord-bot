af78c16156888bf3101eec6cde72c06942134e5d
Mayanhunter187 . <aiden.j.dunaway@gmail.com>
2025-11-19 12:24:11 -0500

    Enable verbose logging for debugging

--- main.py
+++ main.py
@@ -17,23 +17,6 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 # YouTube DL options
 ytdl_format_options = {
     'format': 'bestaudio/best',
-    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
-    'restrictfilenames': True,
-    'noplaylist': True,
-    'nocheckcertificate': True,
-    'ignoreerrors': False,
-    'logtostderr': False,
-    'quiet': True,
-    'no_warnings': True,
-    'default_search': 'auto',
-    'source_address': '0.0.0.0',
-    'cookiefile': '/app/cookies.txt',
-    'source_address': '0.0.0.0',
-    'cookiefile': '/app/cookies.txt'
-}
-
-ffmpeg_options = {
-    'options': '-vn',
     # Reconnect options to handle unstable connections
     'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5'
 }
